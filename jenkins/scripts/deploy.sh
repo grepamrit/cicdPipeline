0fd085dc3a9ec2e8eeb28c76359bad13d1bff4e2
Sushan Kunwar <sushankunwar01@gmail.com>
2020-06-08 10:43:44 +0000

    deploy.sh edited online with Bitbucket

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 echo 'Deploy Finally'
 mysql -u mockup -phellomoco mockup_db < mockup_db.sql
 rm -rf /home/jenkins/deploy/maven-mockup/*
-cp target/test-1.0-SNAPSHOT-jar-with-dependencies.jar /home/jenkins/deploy/maven-mockup
+cp target/mockup-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/jenkins/deploy/maven-mockup
 cp -r etc /home/jenkins/deploy/maven-mockup/
 set -x
 java -jar /home/jenkins/deploy/maven-mockup/mockup-0.0.1-SNAPSHOT-jar-with-dependencies.jar &
