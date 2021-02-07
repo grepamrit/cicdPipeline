1dc418a17ed139f024a6eb7924bc88499276dae3
Sushan Kunwar <sushankunwar01@gmail.com>
2021-02-07 10:48:23 +0000

    kill.sh edited online with Bitbucket

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-ps aux | grep test-1.0-SNAPSHOT-jar-with-dependencies.jar | awk '{print $2}' | xargs kill -9 || true
+ps aux | grep mockup-1.0-SNAPSHOT-jar-with-dependencies.jar | awk '{print $2}' | xargs kill -9 || true
